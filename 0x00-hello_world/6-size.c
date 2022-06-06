33055acb30dcf521226d5e69fbd7fc8e9a642169
Samir Idris <samzidris11@gmail.com>
2022-06-06 09:45:19 -0700

    Completed task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+sizeof(int);
+sizeof(char);
+sizeof(float);
+sizeof(double);
+return (0);
+}
