d6380cfb3db26c3d6f08b3607140e5006dae62a6
Samir Idris <samzidris11@gmail.com>
2022-06-14 23:37:18 -0700

    completed task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int letr;
+
+for (letr = 'a'; letr <= 'z'; letr++)
+{
+putchar("%x", num)
+}
+putchar('\n');
+return (0);
+}
