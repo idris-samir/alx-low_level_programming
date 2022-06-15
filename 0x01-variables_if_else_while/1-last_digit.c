e53e2731699e2d8044e5ffade5fb5c16698b5849
Samir Idris <samzidris11@gmail.com>
2022-06-14 21:06:09 -0700

    completed task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
-		n, lastDigit);
+				n, lastDigit);
 	}
 
 	else if (lastDigit == 0)
@@ -35,7 +35,7 @@ int main(void)
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
- 		n, lastDigit);
+				n, lastDigit);
 	}
 
 	return (0);
