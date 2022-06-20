b5c22ae6d88511ec08d5280238ac5cd573decc7d
Samir Idris <samzidris11@gmail.com>
2022-06-19 22:40:23 -0700

    Completed task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0
  */
+int main(void)
+{
+	int num;
 
-int main(void){
-  int digit, dig;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num  == 9)
+			break;
 
-  for (digit = 0; digit < 10; digit++)
-  {
-    for (dig = 0; dig < 10; dig++)
-    {
-      putchar('0' + digit);
-      putchar('0' + dig);
-      putchar(',');
-      putchar(' ');
-    }
-  }
-  return (0);
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
 }
