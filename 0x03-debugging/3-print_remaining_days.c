35b956a42a9d9cbf25cf0164cf17cb94aae178d3
Samir Idris <samzidris11@gmail.com>
2022-06-22 07:36:13 -0700

    Completed task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,8 +12,17 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+    if ((year % 4 == 0 || year % 400 == 0 || year % 100 == 0)
+    && (((day >=1 && day <= 30)
+    && (month == 4 || month == 6 || month == 9 || month == 11))
+    || ((day >= 1 && day <= 31) 
+    && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+    || (( day >=1 && (day <= 28 || day <= 29))
+    && month == 2)
+    ))
     {
+        if (month >= 1 && month <= 12 && (day >= 1 && day <= 31))
+        {
         if (month >= 2 && day >= 60)
         {
             day++;
@@ -22,9 +31,17 @@ void print_remaining_days(int month, int day, int year)
         printf("Day of the year: %d\n", day);
         printf("Remaining days: %d\n", 366 - day);
     }
+    }
     else
     {
-        if (month == 2 && day == 60)
+        if (month == 2 && day == 60 || (month < 1 && month > 12 && (day < 1 && day > 31)) || 
+        !(((day >=1 && day <= 30)
+    && (month == 4 || month == 6 || month == 9 || month == 11))
+    || ((day >= 1 && day <= 31) 
+    && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+    || (( day >=1 && (day <= 28 || day <= 29))
+    && month == 2)
+    ))
         {
             printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
         }
